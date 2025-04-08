847e00988a799d3f80a728290fcf1fe9c0bdee2a
johan <johanwp97@gmail.com>
2025-04-08 11:36:30 -0500

    useEffect was added to recover the image

--- technique-test/src/App.jsx
+++ technique-test/src/App.jsx
@@ -11,6 +11,8 @@ export function App () {
   // IMPORTANTE al escribir el useEffect, siempre escribir en la función, primero los [], ya que si se olvida, se tendrá un loop infinito.
   // entonces quedaría: useEffect(()=>{},[])
   
+  // Lo ideal es que un useEffect tenga solamente una responsabilidad
+  // Este useEffect es para recuperar la cita al cargar la página
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
@@ -18,20 +20,24 @@ export function App () {
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        // el split permite dividir una cadena de texto a partir del token separador que se le proporcione. Para encontrar el sprint, poner en google "mdn separate string by token"
-        const threeFirstWords = fact.split(' ', 3).join(' ') // el slice es para llamar las tres primera palabras de la frase aleatoria. Y el join las junta en una cadena de texto
-        // alternatica para hacer lo de la línea anterior: fact.split(' ', 3).join(' ')
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}$?fontSize=50&fontColor=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-            console.log(response)
-          })
       })
   }, [])
+
+  // useEffect para recuperar la imagen cada vez que tenemos una vita nueva
+  useEffect(() => {
+    if (!fact) return
+    // el split permite dividir una cadena de texto a partir del token separador que se le proporcione. Para encontrar el sprint, poner en google "mdn separate string by token"
+    const threeFirstWords = fact.split(' ', 3).join(' ') // el slice es para llamar las tres primera palabras de la frase aleatoria. Y el join las junta en una cadena de texto
+    // alternatica para hacer lo de la línea anterior: fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}$?fontSize=50&fontColor=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+        console.log(response)
+      })
+  }, [fact])
 
   return (
     <main>
